0886d1d15628bcd272d84846aad35690a0dec8fe
Vinicius Marques Regitano <116383168+viniciusregitano@users.noreply.github.com>
2024-01-08 09:54:32 -0300

    Update pip-install.sh with bigquery jars
    sparks-bigquery-connector added to starting script

--- pip-install.sh
+++ pip-install.sh
@@ -1,3 +1,3 @@
 python -m pip install delta-spark
 wget  -P /usr/lib/delta/jars/ "https://repo1.maven.org/maven2/io/delta/delta-iceberg_2.13/3.0.0/delta-iceberg_2.13-3.0.0.jar"
-
+wget  -P /usr/lib/delta/jars/ "https://repo1.maven.org/maven2/com/google/cloud/spark/spark-bigquery-connector-common/0.34.0/spark-bigquery-connector-common-0.34.0.jar"
